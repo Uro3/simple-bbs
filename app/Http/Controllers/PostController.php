0ff77f06a8a9035d985013d7c7a9f660b900a8ba
Kazuya Miyakawa <kazuya.miyakawa@person-link.co.jp>
2018-10-20 17:31:00 +0900

    Posts APIを追加

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Post;
+
+class PostController extends Controller {
+  public function index() {
+    return Post::take(5)->get()->keyBy('id');
+  }
+
+  public function store(Request $req) {
+    if ($req->name && $req->message) {
+      $post = new Post;
+      $post->fill($req->all())->save();
+    }
+  }
+
+  public function update($id, Request $req) {
+    Post::find($id)->fill($req->all())->save();
+  }
+
+  public function destroy($id) {
+    Post::destroy($id);
+  }
+}
