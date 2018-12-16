d4123ed7d0fbf2de7307eb03bddaef8ea4243a9d
Kazuya Miyakawa <kazuya.miyakawa@person-link.co.jp>
2018-12-16 22:39:09 +0900

    frontend: vuetifyをインストール

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,8 +1,13 @@
 require('./bootstrap');
 
-import Vue from 'vue'
-import router from './router'
-import store from './store'
+import Vue from 'vue';
+import router from './router';
+import store from './store';
+import Vuetify from 'vuetify';
+import 'vuetify/dist/vuetify.min.css';
+import 'material-design-icons-iconfont/dist/material-design-icons.css';
+
+Vue.use(Vuetify);
 
 const app = new Vue({
     el: '#app',
