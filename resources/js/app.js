d1abe9d4dac8953d30b0e65975f0ced9c6376eeb
Kazuya Miyakawa <kazuya.miyakawa@person-link.co.jp>
2018-12-17 00:15:12 +0900

    frontend: material-design-iconsの導入をCDN経由に変更

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,7 +5,6 @@ import router from './router';
 import store from './store';
 import Vuetify from 'vuetify';
 import 'vuetify/dist/vuetify.min.css';
-import 'material-design-icons-iconfont/dist/material-design-icons.css';
 
 import App from './components/App.vue';
 
--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -9,7 +9,8 @@
     <title>Laravel</title>
 
     <!-- Fonts -->
-    <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css">
+    <!-- <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css"> -->
+    <link href='https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900|Material+Icons' rel="stylesheet">
 
     <script>
       window.Laravel = {
