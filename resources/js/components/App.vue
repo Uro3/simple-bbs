5aac532e8190416b87b45dfe69d1795c33e8754c
Kazuya Miyakawa <kazuya.miyakawa@person-link.co.jp>
2018-12-17 01:05:48 +0900

    ログインページを新規に作成

--- resources/js/components/App.vue
+++ resources/js/components/App.vue
@@ -1,10 +1,24 @@
 <template lang="pug">
-  div#app
-    h1 hello
-    router-view.container
+  div#root
+    v-app
+      h1 hello
+      router-view.container
 </template>
 
 <script>
+import { mapState } from 'vuex';
 export default {
+  computed: {
+    ...mapState('auth', {
+      isLoggedIn: state => state.isLoggedIn,
+    })
+  },
+  created: function() {
+    if (!this.isLoggedIn) {
+      if (!/^\/(signup|login)$/.test(window.location.pathname)) {
+        this.$router.push('/login');
+      }
+    }
+  }
 }
 </script>
