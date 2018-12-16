29834fe9a79d4c68782422227c97975faf7f6678
Kazuya Miyakawa <kazuya.miyakawa@person-link.co.jp>
2018-12-16 21:21:52 +0900

    backend: ThreadControllerを追加

---
+++ app/Http/Controllers/ThreadController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Thread;
+
+class ThreadController extends Controller
+{
+  public function __construct() {
+    $this->middleware('auth:api');
+  }
+
+  public function index() {
+    return Thread::take(50)->get()->keyBy('id');
+  }
+
+  public function store(Request $req) {
+    if ($req->title) {
+      $thread = new Thread;
+      $thread->fill([
+        'owner_id' => $req->user()->id,
+        'title' => $req->title,
+        'description' => $req->description
+      ])->save();
+    }
+  }
+
+}
--- routes/api.php
+++ routes/api.php
@@ -5,5 +5,6 @@ use Illuminate\Http\Request;
 Route::apiResources([
   'posts' => 'PostController',
   'users' => 'UserController',
+  'threads' => 'ThreadController',
   'signup' => 'SignUpController'
 ]);
