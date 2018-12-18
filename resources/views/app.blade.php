ad157cbe06f58e74c9a03775c86a5e594c7eafc2
Kazuya Miyakawa <kazuya.miyakawa@person-link.co.jp>
2018-12-18 20:23:29 +0900

    不要な記述を削除

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,14 +4,8 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- <meta name="csrf-token" content="{{ csrf_token() }}"> -->
-
     <title>弐番</title>
-
-    <!-- Fonts -->
-    <!-- <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css"> -->
     <link href='https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900|Material+Icons' rel="stylesheet">
-
     <script>
       window.Laravel = {
         csrfToken: "{{ csrf_token() }}",
@@ -21,11 +15,7 @@
     </script>
   </head>
   <body>
-    <div id="app">
-      <!-- <div class="container">
-        <router-view></router-view>
-      </div> -->
-    </div>
+    <div id="app"></div>
   </body>
   <script src="{{ mix('js/app.js')}}"></script>
 </html>
